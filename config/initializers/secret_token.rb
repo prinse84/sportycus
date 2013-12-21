3797775cdeab3a06d911e31348b47735842f4f6d
Ray King <raymond.a.king@gmail.com>
2013-12-20 19:48:30 -0600

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sportycus::Application.config.secret_key_base = 'ac8b79799866d0ff2d434305702786a96f81b48ed89e93f9eb45583fae3d48d76e705e7e7679a50222965426a6f1a0dbea36e9afff86739dcff9a2ef4802b797'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sportycus::Application.config.session_store :cookie_store, key: '_sportycus_session'
