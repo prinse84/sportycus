4eec7c3c4c1ae0b4b804290581c2d3af5dfd5120
Ray King <raymond.a.king@gmail.com>
2013-12-24 13:50:37 -0600

    Finish add season

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -5,7 +5,22 @@ class SeasonsController < ApplicationController
   end 
   
   def new
-    
+    @season = Season.new
   end
   
+  def create
+    @season = Season.new(user_params)
+    #@season = Season.new(params[:season])    # Not the final implementation!
+    if @season.save
+      flash[:success] = "Season successfully added"  
+      redirect_to @season
+    else
+      render 'new'
+    end
+  end
+  
+  private
+    def user_params
+      params.require(:season).permit(:title, :description)
+    end
 end
--- spec/requests/season_pages_spec.rb
+++ spec/requests/season_pages_spec.rb
@@ -10,4 +10,25 @@ describe "Season pages" do
     it { should have_content(season.title) }
     it { should have_title(season.title) }
   end
+  
+  describe "add season page" do
+    before { visit new_season_path }
+    let(:submit) { "Add Season" }
+      
+      describe "with invalid information" do
+        it "should not create a season" do
+          expect { click_button submit }.not_to change(Season, :count)
+        end
+      end
+      
+      describe "with valid information" do
+        before do
+          fill_in "Title",  with: "Fall 2005"
+          fill_in "Description",  with: "Fall 2005 Season"
+        end
+        it "should create a season" do
+          expect { click_button submit }.to change(Season, :count).by(1)
+        end
+      end
+  end
 end
