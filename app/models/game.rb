b42a49240262f2e978071c44c4eef4a7ef67d201
Ray King <raymond.a.king@gmail.com>
2014-02-02 07:12:17 -0600

    Working on Stats feature

--- app/models/game.rb
+++ app/models/game.rb
@@ -34,14 +34,28 @@ class Game < ActiveRecord::Base
     end
     return result
   end
-  
+    
   def get_total_box(abbrev)
     stat = Stat.find_by_abbreviation(abbrev)
     if stat.stat_type == 'raw'
-      return self.boxes.where(:stat_id => stat).sum('value')
+      total = self.boxes.where(:stat_id => stat).sum('value')
     elsif stat.stat_type == 'calculated'
-      return 0
+      data = JSON.parse(stat.formula)
+      total = 0      
+      if data["function"] == 'SUM'
+          data["stats"].each do |s|
+            calc_stat = Stat.find_by_unique_identifier(s)
+            total = total + self.boxes.where(:stat_id => calc_stat.id).sum('value')
+          end
+      elsif data["function"] == 'DIV'
+        calc_stat_numerator = self.get_total_box(Stat.find_by_unique_identifier(data["stats"][0]).abbreviation)
+        calc_stat_denumenator = self.get_total_box(Stat.find_by_unique_identifier(data["stats"][1]).abbreviation)
+        if calc_stat_denumenator != 0
+          total = calc_stat_numerator / calc_stat_denumenator
+        end
+      end
     end
+    return total
   end
   
   def get_calculated_field_value_by_player(stat, player)
@@ -60,6 +74,14 @@ class Game < ActiveRecord::Base
       total = 0
     end
     return total
+  end
+  
+  def does_total_runs_match_result?
+    if !get_total_box('R').nil?
+      if get_total_box('R') != self.team_runs
+        true
+      end    
+    end
   end
   
 end
